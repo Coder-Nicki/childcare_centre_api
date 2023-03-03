6e81b2053b63a482f703584c49d2c7a37b5e5cf5
Coder-Nicki <nicki5389@gmail.com>
2023-03-03 11:57:06 +1100

    set up ORM connections and started on models

--- src/main.py
+++ src/main.py
@@ -1,9 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
 
 
 def create_app():
-
+    
+    # creating the flask object
     app = Flask(__name__)
+
+    # configuring app:
+    app.config.from_object("config.app_config")
+
+    db.init_app(app)
+
+
+    from commands import db
 
     @app.route("/")
     def hello():
