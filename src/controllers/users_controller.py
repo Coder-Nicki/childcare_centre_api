d416b67b02b9b5aa117b6746512365e52d1adcf3
Coder-Nicki <nicki5389@gmail.com>
2023-03-04 12:51:39 +1100

    add post route for users

--- src/controllers/users_controller.py
+++ src/controllers/users_controller.py
@@ -1,6 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from models.users import User
 from schemas.users_schema import user_schema, users_schema
+from main import db
 
 user = Blueprint('user', __name__, url_prefix='/users')
 
@@ -8,3 +9,18 @@ user = Blueprint('user', __name__, url_prefix='/users')
 def get_users():
     users = User.query.all()
     return user_schema.dump(users)
+
+
+@user.post("/")
+def create_user():
+    try: 
+        user_fields = user_schema.load(request.json)
+
+        user = User(**user_fields)
+        db.session.add(user)
+        db.session.commit()
+
+    except:
+        return { "message" : "Your information is incorrect"}
+
+    return user_schema.dump(user)
--- src/schemas/users_schema.py
+++ src/schemas/users_schema.py
@@ -1,8 +1,9 @@
 from main import ma
 
 class UserSchema(ma.Schema):
+    
     class Meta:
-        fields: ("id", "username", "password", "admin", "email")
+        fields = ("id", "username", "password", "admin", "email")
 
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
