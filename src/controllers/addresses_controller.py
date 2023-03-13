35d031c66cc1fb669758bd4ffaa245db405588db
Coder-Nicki <nicki5389@gmail.com>
2023-03-13 11:10:01 +1100

    fixed put method in childcare controller to first search for childcare to ensure one exists

--- src/controllers/addresses_controller.py
+++ src/controllers/addresses_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, abort
 from models.addresses import Address
 from schemas.addresses_schema import address_schema, addresses_schema
 from main import db
@@ -13,40 +13,44 @@ def get_address(id):
     return address_schema.dump(address)
     
 
-# Gets an address of a specific childcare centre and returns the address and childcare centre details
-@address.get('/childcare_centre/<int:childcare_centre_id>')
-def get_childcare_address(childcare_centre_id):
-    address = Address.query.filter_by(childcare_centre_id=childcare_centre_id).first()
+# # Gets an address of a specific childcare centre and returns the address and childcare centre details
+# @address.get('/childcare_centre/<int:childcare_centre_id>')
+# def get_childcare_address(childcare_centre_id):
+#     address = Address.query.filter_by(childcare_centre_id=childcare_centre_id).first()
 
-    return address_schema.dump(address)
-
-
-# Gets all the childcare centre ids according to suburb and returns childcare centre details and address
-@address.get('/<string:suburb>')
-def get_childcares_in_a_suburb(suburb):
-    childcares = Address.query.filter_by(suburb=suburb).all()
-
-    return addresses_schema.dump(childcares)
+#     return address_schema.dump(address)
 
 
-# Post an address for a childcare_centre (Childcare_centre must be listed first)
-# then returns the address with specific childcare details
+# # Gets all the childcare centre ids according to suburb and returns childcare centre details and address
+# @address.get('/<string:suburb>')
+# def get_childcares_in_a_suburb(suburb):
+#     childcares = Address.query.filter_by(suburb=suburb).all()
+
+#     return addresses_schema.dump(childcares)
+
+
+# # Post an address for a childcare_centre (Childcare_centre must be listed first)
+# # then returns the address with specific childcare details
 
 @address.post("/")
 # @jwt_required()
 def create_address():
-    # try: 
-    address = Address.query.filter_by(childcare_centre_id=childcare_centre_id).first()
-    address_fields = address_schema.load(request.json)
+    try: 
+        address_fields = address_schema.load(request.json)
+        address = Address.query.filter_by(childcare_centre_id=address_fields["childcare_centre_id"]).first()
 
-    address = Address(**address_fields)
+        if address:
+            # return an abort message to inform the user that a childcare listing already exists for this childcare
+            return abort(400, description="Address already exists for this childcare centre")
 
- 
-    db.session.add(address)
-    db.session.commit()
+        address = Address(**address_fields)
 
-    # except:
-    #     return { "message" : "Your information is incorrect"}
+    
+        db.session.add(address)
+        db.session.commit()
+
+    except:
+        return { "message" : "Your information is incorrect"}
 
     return address_schema.dump(address)
 
