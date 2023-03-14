f0498dd9f851510db3d9a755f44b1fa4d6ae0b85
Coder-Nicki <nicki5389@gmail.com>
2023-03-14 21:19:06 +1100

    Added a cli command for seeding the database

--- src/command/db.py
+++ src/command/db.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 from main import db
+from models.users import User
 
 db_cmd = Blueprint("db", __name__)
 
@@ -7,6 +8,30 @@ db_cmd = Blueprint("db", __name__)
 def create_db():
     db.create_all()
     print('Tables created')
+
+
+@db_cmd.cli.command('seed')
+def seed_db():
+    
+    #Create the users first
+    user1 = User(
+        email = "admin@email.com",
+        password = "123456",
+        admin = True,
+        username = "Saskia"
+    )
+    db.session.add(user1)
+
+    user2 = User(
+        email = "tully@email.com",
+        password = "123456",
+        admin = False,
+        username = "Nala"
+    )
+    db.session.add(user2)
+    db.session.commit()
+    
+    print('Users are seeded')
 
 
 @db_cmd.cli.command('drop')
